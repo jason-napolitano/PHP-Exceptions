84cf692ef19bc2642f91faad1f5f1b2b46361f4a
jason-napolitano <jnapolitanoit@gmail.com>
2019-07-24 23:11:50 -0700

    Added FileSystemTest
    Signed-off-by: jason-napolitano <jnapolitanoit@gmail.com>

---
+++ tests/FileSystem/FileSystemTest.php
@@ -0,0 +1,68 @@
+<?php use PHPUnit\Framework\TestCase;
+
+/**
+ * FileSystemTest Test Case
+ *
+ * This test case is used to ensure that the exceptions within the
+ * Exceptions\FileSystem\* namespace are properly thrown under the
+ * correct set of conditions
+ *
+ * @author  Jason Napolitano <https://github.com/jason-napolitano>
+ * @version 0.0.1
+ * @since   0.0.1
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
+class FileSystemTest extends TestCase
+{
+    /**
+     * Here, we are expecting the \Exceptions\FileSystem\FileNotFoundException
+     * to be thrown
+     *
+     * @return void|mixed
+     */
+    public function testExpectFileNotFoundException()
+    {
+        // We are expecting the Exception to be thrown ...
+        $this->expectException(\Exceptions\FileSystem\FileNotFoundException::class);
+
+        // Let's get the exception to throw
+        $file = __DIR__ . 'assets/image_0.png';
+
+        if ( ! file_exists($file)) {
+            throw new \Exceptions\FileSystem\FileNotFoundException();
+        } else {
+            $file = null;
+        }
+
+        $this->assertNotNull($file);
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Here, we are expecting the \Exceptions\FileSystem\DirectoryAlreadyExistsException
+     * to be thrown
+     *
+     * @return void|mixed
+     */
+    public function testExpectDirectoryAlreadyExistsException()
+    {
+        // We are expecting the Exception to be thrown ...
+        $this->expectException(\Exceptions\FileSystem\DirectoryAlreadyExistsException::class);
+
+        // Let's get the exception to throw
+        $dir = __DIR__ . '/assets';
+
+        if (  file_exists($dir)) {
+            throw new \Exceptions\FileSystem\DirectoryAlreadyExistsException();
+        } else {
+            $dir = false;
+            return $dir;
+        }
+
+        $this->assertTrue($dir);
+
+    }
+
+    //-------------------------------------------------------------------------
+}
