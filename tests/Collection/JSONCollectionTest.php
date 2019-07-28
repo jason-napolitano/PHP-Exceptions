b59801a2e74c7aa70dfd42df1d4354cfc5d99af6
jason-napolitano <jnapolitanoit@gmail.com>
2019-07-28 13:38:41 -0700

    Update JSONCollectionTest
    Signed-off-by: jason-napolitano <jnapolitanoit@gmail.com>

--- tests/Collection/JSONCollectionTest.php
+++ tests/Collection/JSONCollectionTest.php
@@ -32,42 +32,4 @@ class JSONCollectionTest extends TestCase
     }
 
     //-------------------------------------------------------------------------
-
-    /**
-     * @return void
-     */
-    public function testCollectionArrayKeyAlreadyExists(): void
-    {
-        // We are expecting the Exception to be thrown ...
-        $this->expectException(\Exceptions\Collection\ArrayKeyAlreadyExistsException::class);
-
-        // Let's get the exception to throw
-        $var = ['name' => 'Name'];
-
-        if (array_key_exists('name', $var)) {
-            // Throw the exception
-            throw new \Exceptions\Collection\ArrayKeyAlreadyExistsException();
-        }
-    }
-
-    //-------------------------------------------------------------------------
-
-    /**
-     * @return void
-     */
-    public function testCollectionArrayNotExists(): void
-    {
-        // We are expecting the Exception to be thrown ...
-        $this->expectException(\Exceptions\Collection\ArrayNotExistsException::class);
-
-        // Let's get the exception to throw
-        $var = null;
-
-        if ( ! is_array($var)) {
-            // Throw the exception
-            throw new \Exceptions\Collection\ArrayNotExistsException();
-        }
-    }
-
-    //-------------------------------------------------------------------------
 }
