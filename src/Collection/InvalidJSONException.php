ed46883c605d76a1ceadc57c4be6142931d7bf79
jason-napolitano <jnapolitanoit@gmail.com>
2019-07-28 13:25:07 -0700

    Requirements updated
    Now requires PHP >=7.3 and ext-json
    
    Signed-off-by: jason-napolitano <jnapolitanoit@gmail.com>

---
+++ src/Collection/InvalidJSONException.php
@@ -0,0 +1,9 @@
+<?php namespace Exceptions\Collection {
+
+    use Exceptions\ExceptionInterface;
+
+    class InvalidJSONException extends \JsonException implements ExceptionInterface
+    {
+
+    }
+} 
