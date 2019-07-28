95362fb8034c252f6b8b5b7a68c7a1cf54553a1b
jason-napolitano <jnapolitanoit@gmail.com>
2019-07-27 22:46:44 -0700

    Added Exceptions\HTTP
    Signed-off-by: jason-napolitano <jnapolitanoit@gmail.com>

---
+++ src/HTTP/HTTPException.php
@@ -0,0 +1,27 @@
+<?php namespace Exceptions\HTTP {
+
+    use Exceptions\DomainException;
+
+    /**
+     * HTTPException class
+     *
+     * A base class for more generic HTTP request/response related exceptions
+     * to extend
+     *
+     * @package Exceptions\HTTP
+     *
+     * @author  Jason Napolitano <https://github.com/jason-napolitano>
+     * @version 0.0.1
+     * @since   0.0.1
+     * @license MIT <https://opensource.org/licenses/MIT>
+     */
+    class HTTPException extends DomainException
+    {
+        /**
+         * The exception message
+         *
+         * @var string $message The exception message
+         */
+        protected $message = 'An HTTP error was encountered';
+    }
+}
---
+++ tests/HTTP/HTTPTest.php
@@ -0,0 +1,36 @@
+<?php use PHPUnit\Framework\TestCase;
+
+/**
+ * HTTPTest Test Case
+ *
+ * This test case is used to ensure that the exceptions within the
+ * Exceptions\HTTP\* namespace are properly thrown under the correct
+ * set of conditions
+ *
+ * @author  Jason Napolitano <https://github.com/jason-napolitano>
+ * @version 0.0.1
+ * @since   0.0.1
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
+class HTTPTest extends TestCase
+{
+    /**
+     * Here, we are expecting the \Exceptions\HTTP\HTTPException
+     * to be thrown
+     *
+     * @return void|mixed
+     */
+    public function testExpectHTTPException()
+    {
+        // We are expecting the Exception to be thrown ...
+        $this->expectException(\Exceptions\HTTP\HTTPException::class);
+
+        // Simple test
+        if (2 > 1) {
+            throw new \Exceptions\HTTP\HTTPException();
+        }
+
+    }
+
+    //-------------------------------------------------------------------------
+}
