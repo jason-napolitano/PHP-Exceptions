ef76408094d466db542d340e00d964101de79c2c
jason-napolitano <jnapolitanoit@gmail.com>
2019-07-27 22:46:26 -0700

    Added Exceptions\Intl
    Signed-off-by: jason-napolitano <jnapolitanoit@gmail.com>

---
+++ tests/Intl/IntlTest.php
@@ -0,0 +1,36 @@
+<?php use PHPUnit\Framework\TestCase;
+
+/**
+ * IntlTest Test Case
+ *
+ * This test case is used to ensure that the exceptions within the
+ * Exceptions\Intl\* namespace are properly thrown under the correct
+ * set of conditions
+ *
+ * @author  Jason Napolitano <https://github.com/jason-napolitano>
+ * @version 0.0.1
+ * @since   0.0.1
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
+class IntlTest extends TestCase
+{
+    /**
+     * Here, we are expecting the \Exceptions\Intl\IntlException
+     * to be thrown
+     *
+     * @return void|mixed
+     */
+    public function testExpectIntlException()
+    {
+        // We are expecting the Exception to be thrown ...
+        $this->expectException(\Exceptions\Intl\IntlException::class);
+
+        // Simple test
+        if (2 > 1) {
+            throw new \Exceptions\Intl\IntlException();
+        }
+
+    }
+
+    //-------------------------------------------------------------------------
+}
